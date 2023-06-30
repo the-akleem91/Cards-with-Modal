a2401fa29678777648269e742c172fe03104cb8a
Akleem <akleemkhan0208@gmail.com>
2023-06-30 21:31:49 +0530

    Add the folder to the repository

---
+++ src/commitiee.js
@@ -0,0 +1,15 @@
+import "./CommitieeSpeaker.css";
+
+export default function Commitiee() {
+  return (
+    <div className="container">
+      <div className="commitiee-card card">
+        <div className="card-image"></div>
+        <div className="card-content">
+          <h2 className="commitiee-title">Akleem</h2>
+          <h2 className="commitiee-branch">(ECE)</h2>{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
