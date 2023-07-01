0e0a1db9f82ca18ac13272a27570a98becc05b7d
JasnoorMaan <jasnoorm0001@gmail.com>
2023-07-02 01:42:50 +0530

    navbar wasnt working

--- Cards/src/App.jsx
+++ Cards/src/App.jsx
@@ -3,13 +3,12 @@ import './App.css'
 import SpeakerCard from './Components/SpeakerCard'
 import CommitteeCard from './Components/CommitteeCard'
 import SpeakerHeading from './Components/SpeakerHeading'
-import NavbarW from './Components/NavbarW'
+
 
 
 function App() {
   return (
     <>
-    <NavbarW />
     <br></br>
     <br></br>
     <SpeakerHeading />
